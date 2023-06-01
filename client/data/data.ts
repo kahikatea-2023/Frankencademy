d5dbd7695d9970fb3c93de742dc5da389c83cfd0
Jane Zhang <jiejanezhang2012@gmail.com>
2023-06-02 11:12:06 +1200

    changed data order

--- client/data/data.ts
+++ client/data/data.ts
@@ -7,78 +7,6 @@ interface data {
 
 const data: data[] = [
   {
-    id: 13,
-    path: '../images/alex-head.png/',
-    person: 'Alex',
-    bodyPart: 'Head',
-  },
-   {
-    id: 14,
-    path: '../images/alex-torso.png',
-    person: 'Alex',
-    bodyPart: 'Torso',
-  },
-   {
-    id: 15,
-    path: '../images/alex-legs.png/',
-    person: 'Alex',
-    bodyPart: 'Legs',
-  },
-   {
-    id: 16,
-    path: '../images/ahmad-head.png',
-    person: 'Ahmad',
-    bodyPart: 'Head',
-  },
-  {
-    id: 17,
-    path: '../images/ahmad-torso.png',
-    person: 'Ahmad',
-    bodyPart: 'Torso',
-  },
-  {
-    id: 18,
-    path: '../images/ahmad-legs.png',
-    person: 'Ahmad',
-    bodyPart: 'Legs',
-  },
-  {
-    id: 19,
-    path: '../images/jane-legs.png',
-    person: 'Jane',
-    bodyPart: 'Legs',
-  },
-  {
-    id: 20,
-    path: '../images/jane-torso.png',
-    person: 'Jane',
-    bodyPart: 'Torso',
-  },
-  {
-    id: 21,
-    path: '../images/jane-head.png',
-    person: 'Jane',
-    bodyPart: 'Head',
-  },
-  {
-    id: 22,
-    path: '../images/johnny-head.png',
-    person: 'Johnny',
-    bodyPart: 'Head',
-  },
-  {
-    id: 23,
-    path: '../images/johnny-torso.png',
-    person: 'Johnny',
-    bodyPart: 'Torso',
-  },
-  {
-    id: 24,
-    path: '../images/johnny-legs.png',
-    person: 'Johnny',
-    bodyPart: 'Legs',
-  },
-
     id: 1,
     path: '../images/andrew-head.png',
     person: 'Andrew',
@@ -150,4 +78,77 @@ const data: data[] = [
     person: 'Robin',
     bodyPart: 'Legs',
   },
+  {
+    id: 13,
+    path: '../images/alex-head.png/',
+    person: 'Alex',
+    bodyPart: 'Head',
+  },
+   {
+    id: 14,
+    path: '../images/alex-torso.png',
+    person: 'Alex',
+    bodyPart: 'Torso',
+  },
+   {
+    id: 15,
+    path: '../images/alex-legs.png/',
+    person: 'Alex',
+    bodyPart: 'Legs',
+  },
+   {
+    id: 16,
+    path: '../images/ahmad-head.png',
+    person: 'Ahmad',
+    bodyPart: 'Head',
+  },
+  {
+    id: 17,
+    path: '../images/ahmad-torso.png',
+    person: 'Ahmad',
+    bodyPart: 'Torso',
+  },
+  {
+    id: 18,
+    path: '../images/ahmad-legs.png',
+    person: 'Ahmad',
+    bodyPart: 'Legs',
+  },
+  {
+    id: 19,
+    path: '../images/jane-legs.png',
+    person: 'Jane',
+    bodyPart: 'Legs',
+  },
+  {
+    id: 20,
+    path: '../images/jane-torso.png',
+    person: 'Jane',
+    bodyPart: 'Torso',
+  },
+  {
+    id: 21,
+    path: '../images/jane-head.png',
+    person: 'Jane',
+    bodyPart: 'Head',
+  },
+  {
+    id: 22,
+    path: '../images/johnny-head.png',
+    person: 'Johnny',
+    bodyPart: 'Head',
+  },
+  {
+    id: 23,
+    path: '../images/johnny-torso.png',
+    person: 'Johnny',
+    bodyPart: 'Torso',
+  },
+  {
+    id: 24,
+    path: '../images/johnny-legs.png',
+    person: 'Johnny',
+    bodyPart: 'Legs',
+  },
+  
 ]
