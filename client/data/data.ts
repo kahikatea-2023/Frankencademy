1643f33ec5640fa8981789c4a649f1599221494d
Maia <tarapipipim@gmail.com>
2023-06-02 08:45:10 +1200

    completes first 1/2 of data

--- client/data/data.ts
+++ client/data/data.ts
@@ -8,8 +8,74 @@ interface data {
 const data: data[] = [
   {
     id: 1,
-    path: './images/',
-    person: 'Name',
+    path: '../images/andrew-head.png',
+    person: 'Andrew',
     bodyPart: 'Head',
+  },
+  {
+    id: 2,
+    path: '../images/andrew-torso.png',
+    person: 'Andrew',
+    bodyPart: 'Torso',
+  },
+  {
+    id: 3,
+    path: '../images/andrew-legs.png',
+    person: 'Andrew',
+    bodyPart: 'Legs',
+  },
+  {
+    id: 4,
+    path: '../images/daphne-head.png',
+    person: 'Daphne',
+    bodyPart: 'Head',
+  },
+  {
+    id: 5,
+    path: '../images/daphne-torso.png',
+    person: 'Daphne',
+    bodyPart: 'Torso',
+  },
+  {
+    id: 6,
+    path: '../images/daphne-legs.png',
+    person: 'Daphne',
+    bodyPart: 'Legs',
+  },
+  {
+    id: 7,
+    path: '../images/maia-head.png',
+    person: 'Maia',
+    bodyPart: 'Head',
+  },
+  {
+    id: 8,
+    path: '../images/maia-torso.png',
+    person: 'Maia',
+    bodyPart: 'Torso',
+  },
+  {
+    id: 9,
+    path: '../images/maia-legs.png',
+    person: 'Maia',
+    bodyPart: 'Legs',
+  },
+  {
+    id: 10,
+    path: '../images/robin-head.png',
+    person: 'Robin',
+    bodyPart: 'Head',
+  },
+  {
+    id: 11,
+    path: '../images/robin-torso.png',
+    person: 'Robin',
+    bodyPart: 'Torso',
+  },
+  {
+    id: 12,
+    path: '../images/robin-legs.png',
+    person: 'Robin',
+    bodyPart: 'Legs',
   },
 ]
