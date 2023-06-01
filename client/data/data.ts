d8543c1075c49e7961d1a2eb52dd0a29fead4be9
Jane Zhang <jiejanezhang2012@gmail.com>
2023-06-02 11:06:07 +1200

    added data

--- client/data/data.ts
+++ client/data/data.ts
@@ -7,9 +7,76 @@ interface data {
 
 const data: data[] = [
   {
-    id: 1,
-    path: './images/',
-    person: 'Name',
+    id: 13,
+    path: '../images/alex-head.png/',
+    person: 'Alex',
     bodyPart: 'Head',
   },
+   {
+    id: 14,
+    path: '../images/alex-torso.png',
+    person: 'Alex',
+    bodyPart: 'Torso',
+  },
+   {
+    id: 15,
+    path: '../images/alex-legs.png/',
+    person: 'Alex',
+    bodyPart: 'Legs',
+  },
+   {
+    id: 16,
+    path: '../images/ahmad-head.png',
+    person: 'Ahmad',
+    bodyPart: 'Head',
+  },
+  {
+    id: 17,
+    path: '../images/ahmad-torso.png',
+    person: 'Ahmad',
+    bodyPart: 'Torso',
+  },
+  {
+    id: 18,
+    path: '../images/ahmad-legs.png',
+    person: 'Ahmad',
+    bodyPart: 'Legs',
+  },
+  {
+    id: 19,
+    path: '../images/jane-legs.png',
+    person: 'Jane',
+    bodyPart: 'Legs',
+  },
+  {
+    id: 20,
+    path: '../images/jane-torso.png',
+    person: 'Jane',
+    bodyPart: 'Torso',
+  },
+  {
+    id: 21,
+    path: '../images/jane-head.png',
+    person: 'Jane',
+    bodyPart: 'Head',
+  },
+  {
+    id: 22,
+    path: '../images/johnny-head.png',
+    person: 'Johnny',
+    bodyPart: 'Head',
+  },
+  {
+    id: 23,
+    path: '../images/johnny-torso.png',
+    person: 'Johnny',
+    bodyPart: 'Torso',
+  },
+  {
+    id: 24,
+    path: '../images/johnny-legs.png',
+    person: 'Johnny',
+    bodyPart: 'Legs',
+  },
+
 ]
