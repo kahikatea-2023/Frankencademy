07b905a89db208f04201ccaab56325a6426ce06e
Maia <tarapipipim@gmail.com>
2023-06-02 08:10:24 +1200

    sets up data and images folders

---
+++ client/data/data.ts
@@ -0,0 +1,15 @@
+interface data {
+  id: number
+  path: string
+  person: string
+  bodyPart: string
+}
+
+const data: data[] = [
+  {
+    id: 1,
+    path: './images/',
+    person: 'Name',
+    bodyPart: 'Head',
+  },
+]
