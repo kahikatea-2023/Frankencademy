a1d2e2b436006408137938f06bf12960b1fc78d8
Andrew <andrewjtrotter@gmail.com>
2023-06-02 17:25:02 +1200

    removed Audio component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,11 +1,10 @@
 import CharacterBuilder from './CharacterBuilder'
-import Audio from './Audio'
+
 import Fire from './Fire'
 
 function App() {
   return (
     <>
-      <Audio />
       <div>
         <h2 className="title">Frankencademy</h2>
         {/* <p>Build-a-coder</p> */}
